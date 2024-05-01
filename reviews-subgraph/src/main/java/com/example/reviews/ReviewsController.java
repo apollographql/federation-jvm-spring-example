cb060e5f651daae60a4169dd46f2e05836df7592
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-05-01 11:13:22 +0100

    Make use of Spring for GraphQL federation support

--- reviews-subgraph/src/main/java/com/example/reviews/ReviewsController.java
+++ reviews-subgraph/src/main/java/com/example/reviews/ReviewsController.java
@@ -6,11 +6,18 @@ import com.example.reviews.model.Product;
 import com.example.reviews.model.Review;
 import com.example.reviews.model.ReviewSource;
 
+import org.springframework.graphql.data.federation.EntityMapping;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReviewsController {
+
+  @EntityMapping
+  public Product product(@Argument Long id) {
+    return new Product(id);
+  }
 
   @SchemaMapping
   public List<Review> reviews(Product product) {
